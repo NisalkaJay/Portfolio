bf0df2423ecbeaa1a8ef96efc99c641fc060caff
Nisalka Jayasinghe <jayasinghenisalka@gmail.com>
2024-08-16 04:33:29 +0530

    Update Navbar component and add new CV file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from '../css/Navbar.module.css';
 import menuIcon from '../assets/menu.svg';
 import closeIcon from '../assets/close.svg';
-import MyCV from '../files/blender.pdf'
+import MyCV from '../files/Nisalka_jayasinghe_CV.pdf'
 
 const navLinks = [
   { id: 'home', title: 'Home' },
