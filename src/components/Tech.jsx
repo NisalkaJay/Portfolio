8ea72c5c4fd0fa1413a7c1b34abb7b03730ef9e9
Nisalka Jayasinghe <jayasinghenisalka@gmail.com>
2024-08-15 22:45:40 +0530

    Updated index.html and Tech.jsx with new modifications

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import styles from '../css/tech.module.css';
 
+import figma from '../assets/Figma_.png';
+import react from '../assets/React_.png';
+import css from '../assets/Css3_.png';
+import javascript from '../assets/Javascript_.png';
+import html from '../assets/Html5_.png';
+import github from '../assets/Github_.png';
+import blender from '../assets/blender.png';
+import tailwind from '../assets/Tailwind_.png';
+
+
 const Tech = () => {
   return (
     <div id="tech" className={styles.section}>
@@ -10,14 +20,14 @@ const Tech = () => {
       </div>
       <div className={styles.banner}>
         <div className={styles.slider} style={{ '--quantity': 8 }}>
-          <div className={styles.item} style={{ '--position': 1 }}><img src="src\assets\Figma_.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 2 }}><img src="src\assets\React_.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 3 }}><img src="src\assets\Css3_.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 4 }}><img src="src\assets\Javascript_.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 5 }}><img src="src\assets\Html5_.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 6 }}><img src="src\assets\Github_.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 7 }}><img src="src\assets\blender.png" alt=''/></div>
-          <div className={styles.item} style={{ '--position': 8 }}><img src="src\assets\Tailwind_.png" alt=''/></div>
+          <div className={styles.item} style={{ '--position': 1 }}><img src={figma} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 2 }}><img src={react} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 3 }}><img src={css} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 4 }}><img src={javascript} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 5 }}><img src={html} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 6 }}><img src={github} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 7 }}><img src={blender} alt=''/></div>
+          <div className={styles.item} style={{ '--position': 8 }}><img src={tailwind} alt=''/></div>
         </div>
       </div>
     </div>
